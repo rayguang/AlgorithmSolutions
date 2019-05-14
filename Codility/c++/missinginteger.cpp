69d6f42df4eefd7743f80d2270e7820583d11823
Ray <ray@cryptoterminal.io>
2019-05-14 12:56:05 -0400

    updates

--- Codility/c++/missinginteger.cpp
+++ Codility/c++/missinginteger.cpp
@@ -4,39 +4,29 @@
 
 using namespace std;
 
-int solution(vector<int> &A) {
-    size_t size = A.size() + 1;
+int solution (vector<int> &A)
+{
     map<int, bool> occurs;
+    size_t size = A.size()+1;
 
-    for (size_t i = 0; i < size; ++i) {
-        if (A[i] > 0 && A[i] <= A.size()) {
-            // std::cout << "i: " << i << " A[i]: " << A[i] << "  A.size(): " << A.size() << std::endl;
-            occurs[A[i] - 1] = true;
+    for (int i = 0; i < size ; ++i)
+    {
+        if (A[i] > 0 ){
+            occurs[A[i]-1]=true;
         }
     }
 
-    for (auto it : occurs)
-        std::cout << it.first+1 << " " << it.second << std::endl;
-
-    for (size_t i = 0; i < size; ++i) {
+    for ( size_t i = 0; i < size ; ++i){
         if (occurs[i] == false)
-            return i + 1;
+            return i+1;
     }
-
-    return 0;
 }
 
-
-
-int main ()
-{
-    int arr[] = { -1, -2, 0, 1, 2, 4, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
+int main(){
+    int arr[] = {-1, -2, 3,4,1,7};
+    size_t n = sizeof(arr)/sizeof(arr[0]);
     vector<int> a(arr, arr+n);
-    for (int itr : a)
-        std::cout << itr << std::endl;
-    
-    std::cout << solution(a) << std::endl;
-}
 
+    std::cout << "solution: " << solution(a)  << std::endl;
+
+}
