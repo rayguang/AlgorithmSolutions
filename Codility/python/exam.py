ec97f85201812967bb3380bba6de41bdb223eab6
Ray <ray@cryptoterminal.io>
2019-06-07 14:29:25 -0400

    updates

---
+++ Codility/python/exam.py
@@ -0,0 +1,13 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N):
+    # write your code in Python 3.6
+    # use set for unique elements
+    if N < 1 or N > 100:
+        print("N is INVALID")
+        pass
+    
+    pass
+
+print(solution(0))
---
+++ hackerrank/countingvalleys.py
@@ -0,0 +1,30 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def counting_valleys(n, s):
+    l = [ for i in s if i == 'U']
+    for i in range(0, n+1):
+        if s[i] == 'u'
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    s = input()
+
+    result = counting_valleys(n, s)
+    print(result)
+
+
+
+
+
+
+
+
+
+
